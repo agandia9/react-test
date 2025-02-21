e34ed50de10d32bad0af47e8b49b13b73e913a5f
Alejandro Delgado <alejandro.gandia9@gmail.com>
2025-02-21 17:26:18 +0100

    added product type file

---
+++ src/types/Product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+    id: number;
+    name: string;
+    size: string;
+    color: string;
+    material: string;
+    type: string;
+    category: string;
+    options: {
+      outOfStock: boolean;
+      waterproof: boolean;
+      original: boolean;
+    };
+  }
