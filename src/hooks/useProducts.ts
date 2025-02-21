90938c6f3ec75f8618dfe830946ef5daedbce70f
Alejandro Delgado <alejandro.gandia9@gmail.com>
2025-02-21 23:52:41 +0100

    filter products ok

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -7,12 +7,17 @@ import { SortOrder } from "../types/SortOrder";
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [filters, setFilters] = useState<Filters>({ material: "", category: "", color: "", type: "" });
-  const [sortOrder, setSortOrder] = useState<SortOrder>(null);
+  const [sortOrder, setSortOrder] = useState<SortOrder>(SortOrder.NONE);
 
   useEffect(() => {
     fetchProducts().then(setProducts);
   }, []);
 
-  const filteredProducts = products;
+  const filteredProducts = products
+  .filter((p) => Object.entries(filters).every(([key, value]) => !value || p[key as keyof Filters] === value))
+  .sort((a, b) => {
+    if (!sortOrder) return 0;
+    return sortOrder === SortOrder.ASC ? a.size.localeCompare(b.size) : b.size.localeCompare(a.size);
+  });
   return { filteredProducts, setFilters, setSortOrder };
 };
