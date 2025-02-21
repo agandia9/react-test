431a6d0bbbe68681cd28686c149bc479cbc6de50
Alejandro Delgado <alejandro.gandia9@gmail.com>
2025-02-21 23:53:05 +0100

    trying to get json with fetch api

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,6 +3,7 @@ import { Product } from "../types/Product";
 export const fetchProducts = async (): Promise<Product[]> => {
   try {
     const response = await fetch("/data/products.json");
+    console.log(response)
     return await response.json();
   } catch (error) {
     console.error("Error fetching products:", error);
