37845f7a9ea2f6cd7bf948c93779583fcdeb8163
Alejandro Delgado <alejandro.gandia9@gmail.com>
2025-02-21 17:50:31 +0100

    sort order type in separate file

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import { Product } from "../types/Product";
 import { fetchProducts } from "../services/productService";
 import { Filters } from "../types/Filters";
+import { SortOrder } from "../types/SortOrder";
 
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [filters, setFilters] = useState<Filters>({ material: "", category: "", color: "", type: "" });
-  const [sortOrder, setSortOrder] = useState<"asc" | "desc" | null>(null);
+  const [sortOrder, setSortOrder] = useState<SortOrder>(null);
 
   useEffect(() => {
     fetchProducts().then(setProducts);
