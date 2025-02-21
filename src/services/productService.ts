9555c5d3c3efb82e19b3e50a622750cdb1700ce8
Alejandro Delgado <alejandro.gandia9@gmail.com>
2025-02-21 17:28:31 +0100

    fetch products and created service folder

---
+++ src/services/productService.ts
@@ -0,0 +1,11 @@
+import { Product } from "../types/Product";
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  try {
+    const response = await fetch("/data/products.json");
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+};
